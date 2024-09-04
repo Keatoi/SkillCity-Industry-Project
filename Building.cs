25d71951e4aaaaac2d7a4960e2f1f515a5a69452
CodePortfolioMayron <123511205+CodePortfolioMayron@users.noreply.github.com>
2024-09-04 20:12:13 +0100

    Add files via upload

---
+++ Building.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.XR;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public GameObject strucprefab;
+    public GameObject strucprefabghost;
+    public Camera head;
+    public float rotationspeed= 5f;
+    public bool isbuilding = false;
+
+    private GameObject currentghost;
+
+
+
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Ray Pos = head.ScreenPointToRay(mouse);
+        RaycastHit hit;
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            isbuilding = !isbuilding;
+        }
+
+        if (isbuilding)
+        {
+
+            if (currentghost == null)
+            {
+                currentghost = Instantiate(strucprefabghost);
+
+            }    
+
+            if (Physics.Raycast(Pos, out hit, 100))
+                {
+                    Debug.Log("hit");
+                Vector3 prefabSize = strucprefabghost.transform.localScale;
+                Vector3 offset = hit.normal * (Mathf.Max(prefabSize.x, prefabSize.y, prefabSize.z) / 2);
+
+                Vector3 buildpos = hit.point+ offset;
+
+                if (currentghost != null)
+                {
+                    currentghost.transform.position = buildpos; 
+
+                }
+                if (Input.mouseScrollDelta.y != 0)
+                {
+                    float rotation = Input.mouseScrollDelta.y;
+                    currentghost.transform.eulerAngles += new Vector3(0, rotation * rotationspeed, 0);
+                }
+
+
+
+
+                if (Input.GetMouseButtonDown(0))
+                    {
+                        Instantiate(strucprefab, currentghost.transform.position, currentghost.transform.rotation);
+                        Destroy(currentghost);
+                        currentghost = null;
+                    }
+                }
+
+
+        }
+        else
+        {
+            if (currentghost != null) { Destroy(currentghost); }
+        }
+
+    }
+}
+
