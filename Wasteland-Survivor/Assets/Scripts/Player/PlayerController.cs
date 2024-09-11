5baa261e212726b8ef33e597c708d99c281bbd5f
Owen Atkinson <45954357+Keatoi@users.noreply.github.com>
2024-09-11 11:15:40 +0100

    Player Prefab with Basic Movement
    -Player prefab consists of character controller, camera and placeholder mesh.
    -Currently able to move and look around, with configurable speed and mouse sensitivity
    -TODO: Jumping, Crouching, RE-style spin, etc.

--- Wasteland-Survivor/Assets/Scripts/Player/PlayerController.cs
+++ Wasteland-Survivor/Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //Input variables
+    private Vector2 moveInput;
+    public float gravity = -9.81f;
+    private bool bisGrounded;
+    [SerializeField] private Camera mainCam;
+    [SerializeField]Transform playerBodyPos;
+    
+    private CharacterController controller;
+    
+    float xRotation = 0;
+    
+    [SerializeField]float minViewRotation = -25f;//Limit how much the player can look down
+    [SerializeField] float walkSpeed = 20f;
+    private float speed;
+    //MouseSettings
+    public float mouseSensitivity = 100f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        speed = walkSpeed;
+    }
+    private void OnLook(InputValue value)
+    {
+        //Get and clamp look values
+        Vector2 look = value.Get<Vector2>();
+        float lookX = look.x * mouseSensitivity * Time.deltaTime;
+        float lookY = look.y * mouseSensitivity * Time.deltaTime;
+        xRotation -= lookY;
+        xRotation = Mathf.Clamp(xRotation,-90f,minViewRotation);
+        //Rotate camera up
+        mainCam.transform.localRotation = Quaternion.Euler(xRotation,0,0);
+        //Rotate player entity
+        transform.Rotate(Vector3.up * lookX);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMove(InputValue value)
     {
-        
+        moveInput = value.Get<Vector2>();
+
+    }
+    void Movement()
+    {
+        Vector3 moveVelocity = transform.right * moveInput.x + transform.forward * moveInput.y;
+        bisGrounded = controller.isGrounded;
+
+        //Gravity Implementation
+        moveVelocity.y += gravity * Time.deltaTime;
+        if (controller.isGrounded)
+        {
+            moveVelocity.y = 0;
+        }
+        else
+        {
+            moveVelocity.y += gravity * Time.deltaTime;
+        }
+        //FWD/Strafing movement
+       // if (moveVelocity != Vector3.zero) { transform.forward = moveVelocity; }
+        controller.Move(moveVelocity * speed * Time.deltaTime);
+       
+       
+    }
+   
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
     }
 }
