e0194db732d554293334ef796c457bfe2ee1e4cc
Owen Atkinson <45954357+Keatoi@users.noreply.github.com>
2024-09-30 00:58:45 +0100

    FPS Arms Assets, Sword Assets and placeholder crosshair

--- Wasteland-Survivor/Assets/Scripts/Other/Weapons/MeleeSystem.cs
+++ Wasteland-Survivor/Assets/Scripts/Other/Weapons/MeleeSystem.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class MeleeSystem : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float HorizontalRange = 6f;
-    public float VerticalRange = 6f;
-    public float DepthRange = 3f;
+    public bool bCanAttack = true;
     RaycastHit m_hit;
     bool m_hasHit;
     void Start()
@@ -22,12 +20,7 @@ public class MeleeSystem : MonoBehaviour
     }
     public void Attack()
     {
-        Vector3 halfExtent =  new Vector3 (HorizontalRange, VerticalRange, DepthRange);
-        Vector3 centre = transform.forward * (DepthRange / 2);
-        m_hasHit =  Physics.BoxCast(centre, halfExtent, transform.forward, out m_hit);
-        if(m_hasHit )
-        {
-            Debug.Log(m_hit.collider);
-        }
+        if (bCanAttack) { return; }
+
     }
 }
