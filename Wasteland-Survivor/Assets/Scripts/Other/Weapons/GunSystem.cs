69abecd3d5312a99248cfca7e75e1b3ef24ccb9d
Owen Atkinson <45954357+Keatoi@users.noreply.github.com>
2024-10-22 15:33:17 +0100

    Gun Audio Fixed

--- Wasteland-Survivor/Assets/Scripts/Other/Weapons/GunSystem.cs
+++ Wasteland-Survivor/Assets/Scripts/Other/Weapons/GunSystem.cs
@@ -20,21 +20,29 @@ public class GunSystem : MonoBehaviour
     public GameObject hitObject;
     public AudioClip[] GunSFXArr;
     public AudioClip reloadSFX;
-    public float GunShotVolume;
-    public float ReloadVolume;
+    public float GunShotVolume = 1.0f;
+    public float ReloadVolume = 1.0f;
     private AudioSource m_Audio;
 
     // Start is called before the first frame update
     void Start()
     {
         m_Audio = GetComponent<AudioSource>();
-        if (m_Audio != null)
+        if (m_Audio == null)
         {
-            Debug.Log("No AudioSource Found!");
+            Debug.Log("No AudioSource Found!Creating Audiosource");
+            m_Audio = gameObject.AddComponent<AudioSource>();
+
+        }
+        if(m_Audio != null)
+        {
+           // Debug.Log(GunSFXArr.Length);
+            m_Audio.clip = reloadSFX;
+            m_Audio.volume = GunShotVolume;
         }
         currentRounds = maxMagazineSize;
     }
-
+   
     // Update is called once per frame
     void Update()
     {
@@ -52,7 +60,7 @@ public class GunSystem : MonoBehaviour
     {
         //TODO ADD FLASH AND BULLET MARKERS
         //Raycast from gun barrel/camera to destination
-        Debug.Log("Firing handgunne");
+        
         RaycastHit hit;
 
         Vector3 origin = barrelCamera.transform.position;
@@ -70,27 +78,35 @@ public class GunSystem : MonoBehaviour
             dir.z += RandDirZ;
 
         }
-       
-        
-
-        if (Physics.Raycast(origin, dir, out hit, range) && currentRounds > 0)
+        if(currentRounds > 0)
         {
-            //If we have ammo then play SFX and if the hit collider has the health component then subtract health
-            Debug.DrawLine(origin, dir);//Only works in scene view, so the player won't see this
+            //Debug.Log("Firing handgun");
             PlayGunSFX();
-            if (hit.collider != null)
-            {
-                hitObject = hit.collider.gameObject;
-                Debug.Log(hitObject.name);
-                if (hitObject.GetComponent<HealthSystem>() != null && hitObject.CompareTag("Enemy"))
-                {
-                    Debug.Log(hitObject.name + "Has been hurt!");
-                    hitObject.GetComponent<HealthSystem>().ChangeHealth(-damage);
-                }
-            }
             //Decrement ammo from magazine
             currentRounds--;
+            Debug.Log(currentRounds);
+            if (Physics.Raycast(origin, dir, out hit, range))
+            {
+                //If we have ammo then play SFX and if the hit collider has the health component then subtract health
+                Debug.DrawLine(origin, dir);//Only works in scene view, so the player won't see this
+
+                if (hit.collider != null)
+                {
+                    hitObject = hit.collider.gameObject;
+                    Debug.Log(hitObject.name);
+                    if (hitObject.GetComponent<HealthSystem>() != null && hitObject.CompareTag("Enemy"))
+                    {
+                        Debug.Log(hitObject.name + "Has been hurt!");
+                        hitObject.GetComponent<HealthSystem>().ChangeHealth(-damage);
+                    }
+                }
+
+            }
         }
+        
+
+
+
 
     }
     public void Reload()
@@ -141,15 +157,19 @@ public class GunSystem : MonoBehaviour
     }
     private void PlayGunSFX()
     {
-        if(currentRounds != 0)
+        Debug.Log("Playing Sound");
+        if(currentRounds > 0)
         {
             //shoot gun sound
+            
             m_Audio.clip = GunSFXArr[0];
+            Debug.Log(m_Audio.clip.name);
         }
         else 
         {
             //empty gun sound
             m_Audio.clip = GunSFXArr[1];
+            Debug.Log(m_Audio.clip.name);
         }
         //play the sound
         m_Audio.PlayOneShot(m_Audio.clip);
