6624017737f54db3d849d0ff2339c92c68342b8c
Owen Atkinson <45954357+Keatoi@users.noreply.github.com>
2024-10-23 13:59:12 +0100

    Generic Objective

--- Wasteland-Survivor/Assets/Scripts/Other/Objectives/Director.cs
+++ Wasteland-Survivor/Assets/Scripts/Other/Objectives/Director.cs
@@ -14,11 +14,16 @@ public class Director : MonoBehaviour
         {
             objectiveManager = gameObject.AddComponent<ObjectiveManager>();
         }
+        //Non Specified Objective (Completion check must be done in the objective object. Ex. Build a building then call the func for completing the corresponding objective once that building is built
+        objectiveManager.AddObjective(new GenericObjective("Test Objective", "Do a testing thing"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            objectiveManager.CompleteObjective("Test Objective");
+        }
     }
 }
---
+++ Wasteland-Survivor/Assets/Scripts/Other/Objectives/GenericObjective.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericObjective : Objective
+{
+   public GenericObjective(string title, string description)
+    {
+        Title = title;
+        Description = description;
+        IsCompleted = false;
+    }
+    public override void CompleteObjective()
+    {
+        IsCompleted = true;
+    }
+}
