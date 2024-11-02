efe2ce582a8f7615c399d82ed4bb45fc1135aefb
CodePortfolioMayron <Teschimam@outlook.com>
2024-11-02 16:22:15 +0000

    file management and casingpool parent

--- Wasteland-Survivor/Assets/Scripts/Other/Weapons/ShellPool.cs
+++ Wasteland-Survivor/Assets/Scripts/Other/Weapons/ShellPool.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class ShellPool : MonoBehaviour
 {
     public GameObject casingPrefab;
+    private GameObject CasingPool;
     public int poolSize = 15;
     private Queue<GameObject> objPool = new Queue<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
+        CasingPool = new GameObject("Casings pool");
       //Init pool
       for (int i = 0; i < poolSize; i++)
         {
             GameObject go = Instantiate(casingPrefab);
+            go.transform.parent = CasingPool.transform;
             go.SetActive(false);
             objPool.Enqueue(go);
         }
